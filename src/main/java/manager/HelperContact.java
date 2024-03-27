8d9981c3928cb5843489b48331d40aae50ff366f
lonchik2020 <krasleo@gmail.com>
2024-03-27 20:54:19 +0200

    add new contact success update

--- src/main/java/manager/HelperContact.java
+++ src/main/java/manager/HelperContact.java
@@ -1,18 +1,52 @@
 package manager;
 
+import models.Contact;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class HelperContact extends HelperBase{
     public HelperContact(WebDriver wd) {
         super(wd);
     }
 
-    public void clickAddButton() {
+    public void openContactForm() {
         click(By.xpath("//a[text()='ADD']"));
     }
 
-    public void fillContactForm() {
-        //type(By.xpath("//input[@placeholder='Name']"),);
+    public void fillContactForm(Contact contact) {
+        type(By.xpath("//input[@placeholder='Name']"), contact.getName());
+        type(By.xpath("//input[@placeholder='Last Name']"), contact.getLastName());
+        type(By.xpath("//input[@placeholder='Phone']"), contact.getPhone());
+        type(By.xpath("//input[@placeholder='email']"), contact.getEmail());
+        type(By.xpath("//input[@placeholder='Address']"), contact.getAddress());
+        type(By.xpath("//input[@placeholder='description']"), contact.getDescription());
+    }
+
+    public void saveContact() {
+        click(By.cssSelector(".add_form__2rsm2>button"));
+    }
+
+    public boolean isContactAddedByName(String name) {
+        List<WebElement>list = wd.findElements(By.cssSelector("h2"));
+        for(WebElement element:list){
+            if(element.getText().equals(name)){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public boolean isContactAddedByPhone(String phone) {
+        List<WebElement>list = wd.findElements(By.cssSelector("h3"));
+        for(WebElement element:list){
+            if(element.getText().equals(phone)){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/tests/AddNewContactTests.java
+++ src/test/java/tests/AddNewContactTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import models.Contact;
 import models.User;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,21 +17,66 @@ public class AddNewContactTests extends TestBase{
     }
 
     @Test
-    public void addNewContactSuccess(){
+    public void addNewContactSuccessAllFields(){
         int y = (int)(System.currentTimeMillis()/1000)%3600;
         Contact contact = Contact.builder()
                 .name("Cristiano")
                 .lastName("Ronaldo")
-                .phone("5555555")
-                .email("cristiano"+y+"@gmail.com")
+                .phone("5555555" + y)
+                .email("cristiano"+ y +"@gmail.com")
                 .address("Portugal,Lisbon")
                 .description("football player")
                 .build();
+        app.getHelperContact().openContactForm();
+        app.getHelperContact().fillContactForm(contact);
+        app.getHelperContact().saveContact();
+        Assert.assertTrue(app.getHelperContact().isContactAddedByName(contact.getName()));
+        Assert.assertTrue(app.getHelperContact().isContactAddedByPhone(contact.getPhone()));
+
+    }
+
+    @Test
+    public void addNewContactSuccessRequiredFields(){
+        int y = (int)(System.currentTimeMillis()/1000)%3600;
+        Contact contact = Contact.builder()
+                .name("Cristiano" + y)
+                .lastName("Ronaldo")
+                .phone("5555555" + y)
+                .email("cristiano"+y+"@gmail.com")
+                .address("Portugal,Lisbon")
+                .build();
+
+        app.getHelperContact().openContactForm();
+        app.getHelperContact().fillContactForm(contact);
+        app.getHelperContact().saveContact();
+        Assert.assertTrue(app.getHelperContact().isContactAddedByName(contact.getName()));
+        Assert.assertTrue(app.getHelperContact().isContactAddedByPhone(contact.getPhone()));
+
+    }
+
+    @Test
+    public void addNewContactWrongName(){
+
+    }
 
 
-        app.getHelperContact().clickAddButton();
-        app.getHelperContact().fillContactForm();
-        //app.getHelperContact().clickSaveButton();
+    @Test
+    public void addNewContactWrongAddress(){
+
+    }
+
+    @Test
+    public void addNewContactWrongLastName(){
+
+    }
+
+    @Test
+    public void addNewContactWrongPhone(){
+
+    }
+
+    @Test
+    public void addNewContactWrongEmail(){
 
     }
 
