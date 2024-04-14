fb4a02d47bd0a9f509967a6cd640b8eacd222f9a
lonchik2020 <krasleo@gmail.com>
2024-04-14 20:36:04 +0300

    added browser and suite

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -2,6 +2,9 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +20,26 @@ public class ApplicationManager {
 
     HelperContact helperContact;
 
+    String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init(){
+        if(browser.equals(BrowserType.CHROME)){
+            wd = new EventFiringWebDriver(new ChromeDriver());
+            logger.info("All tests run in Chrome Browser");
+        }else if(browser.equals(BrowserType.FIREFOX)){
+            wd = new EventFiringWebDriver(new FirefoxDriver());
+            logger.info("All tests run in FIREFOX Browser");
+        }else if(browser.equals(BrowserType.EDGE)){
+            wd = new EventFiringWebDriver(new EdgeDriver());
+            logger.info("All tests run in EDGE Browser");
+        }
+
         //wd = new ChromeDriver();
-        wd = new EventFiringWebDriver(new ChromeDriver());
-        logger.info("All tests run in Chrome Browser");
+
         wd.navigate().to("https://telranedu.web.app/home");
         logger.info("The link--->" +wd.getCurrentUrl());
         wd.manage().window().maximize();
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
@@ -18,14 +19,15 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    static ApplicationManager app = new ApplicationManager();
+    static ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp(){
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown(){
         app.stop();
     }
